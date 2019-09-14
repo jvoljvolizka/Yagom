1599c774de279f72a35f5a1bea7bdd8116fc5846
jvol jvolizka <jvoljvolizka@gmail.com>
2019-09-14 15:22:29 +0300

    ultrasecure aes creation

--- Yagom.go
+++ Yagom.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha1"
 	"crypto/x509"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"os"
 
 	"github.com/akamensky/argparse"
+	"golang.org/x/crypto/pbkdf2"
 )
 
 const (
-	debug = true //debug switch
+	debug               = true //debug switch
+	ultrasecurepassword = true //if this is true create the salt value using turkish lira to dolar exchange rate
 )
-
-var exPublicKey = ""
 
 //**********RSA FUNCTIONS
 
@@ -44,6 +54,7 @@ func exportRsaPublicKeyAsPemStr(pubkey *rsa.PublicKey) (string, error) {
 func parseRsaPublicKeyFromPemStr(pubPEM string) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode([]byte(pubPEM))
 	if block == nil {
+
 		return nil, errors.New("failed to parse PEM block containing the key")
 	}
 
@@ -62,6 +73,102 @@ func parseRsaPublicKeyFromPemStr(pubPEM string) (*rsa.PublicKey, error) {
 }
 
 //***********RSA FUNCTIONS END
+
+//*****AES STUFF
+
+//i stole it from https://www.thepolyglotdeveloper.com/2018/02/encrypt-decrypt-data-golang-application-crypto-packages/
+//but for the sake of every holy goddess of technology don't ever use md5 for aes key derivation this is just plain stupid
+/*func createHash(key string) string {
+	hasher := md5.New() just no
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}*/
+
+func encrypt(data []byte, passphrase string, salt []byte) []byte {
+	block, _ := aes.NewCipher(pbkdf2.Key([]byte(passphrase), salt, 4096, 32, sha1.New))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func decrypt(data []byte, passphrase string, salt []byte) []byte {
+	key := pbkdf2.Key([]byte(passphrase), salt, 4096, 32, sha1.New)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	return plaintext
+}
+
+//*****AES STUFF END
+
+//AES password and salt create
+func createPass() (string, []byte) {
+	password := make([]byte, 64)
+	salt := make([]byte, 8)
+	/*
+
+
+	 */
+	if ultrasecurepassword {
+		//things that i did for just a stupid joke
+		response, err := http.Get("https://api.exchangeratesapi.io/latest?base=USD&symbols=USD,TRY")
+		if err != nil {
+			fmt.Println("The HTTP request failed fall back to sane salt creation", err)
+			_, err := rand.Read(salt)
+			if err != nil {
+				fmt.Println("error:", err)
+			}
+
+		} else {
+			type Tree struct {
+				Value map[string]float32 `json:"rates"`
+			}
+			data, _ := ioutil.ReadAll(response.Body)
+			fmt.Println(string(data))
+			var obj Tree
+			json.Unmarshal(data, &obj)
+			buf := new(bytes.Buffer)
+
+			err := binary.Write(buf, binary.LittleEndian, obj.Value["TRY"])
+			if err != nil {
+				fmt.Println("binary.Write failed:", err)
+			}
+		}
+
+		/**/
+
+	} else {
+		_, err := rand.Read(salt)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+	}
+
+	_, err := rand.Read(password)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(password), salt
+}
 
 func main() {
 	// Create new parser object
@@ -126,35 +233,43 @@ func server(ConnHost, ConnPort string) {
 
 // Handles incoming requests.
 func serverreq(conn net.Conn) {
-	// Make a buffer to hold incoming data.
-	buf := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
-	len, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-	}
-
-	//fmt.Println(buf)
-	message := string(buf[:len])
-	if message[:8] == "dalyarak" {
-		if message[8:] != "" { //init handshake and update the public key
-			exPublicKey = message[8:]
-			pubkey, err := parseRsaPublicKeyFromPemStr(exPublicKey)
-			if err != nil {
-				fmt.Println("pubkey error: ", err.Error())
-				os.Exit(1)
+	exPublicKey := ""
+	for {
+		defer conn.Close()
+		// Make a buffer to hold incoming data.
+		buf := make([]byte, 1024)
+		// Read the incoming connection into the buffer.
+		len, err := conn.Read(buf)
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println(conn.RemoteAddr().String() + " closed the connection")
+				conn.Close()
+				return
 			}
-			fmt.Println(*pubkey)
+			fmt.Println("Error reading:", err.Error())
 		}
-		fmt.Println(exPublicKey)
-		conn.Write([]byte("got it\n"))
 
+		message := string(buf[:len])
+		if len > 8 && message[:8] == "dalyarak" {
+			if message[8:] != "" { //init handshake and update the public key
+				exPublicKey = message[8:]
+				pubkey, err := parseRsaPublicKeyFromPemStr(exPublicKey)
+
+				if err != nil {
+					fmt.Println("pubkey error: ", err.Error())
+					os.Exit(1)
+				}
+				fmt.Println(*pubkey)
+			}
+			fmt.Println(exPublicKey)
+			conn.Write([]byte("got it\n"))
+
+		}
+		fmt.Println(message)
 	}
-	//fmt.Println(message)
-	// Close the connection when you're done with it.
-	conn.Close()
 }
 
+//this is the funtion that actually does the monitoring job
 func listendata(key string) {
 
 }
@@ -183,16 +298,16 @@ func client(ConnHost, ConnPort string) {
 	//use minimum 2048 for rsa
 	_, pubkey := generateRsaKeyPair(2048)
 
-	exppub, err := exportRsaPublicKeyAsPemStr(pubkey)
+	exClientPub, err := exportRsaPublicKeyAsPemStr(pubkey)
 
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	keyStr += exppub
+	keyStr += exClientPub
 
-	fmt.Println(exppub)
+	fmt.Println(exClientPub)
 	_, err = c.Write([]byte(keyStr))
 
 	if err != nil {
